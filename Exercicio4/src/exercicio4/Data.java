ae35f744d4b3f78739e8f057004898ae0050edc0
Sofia <sppessil@inf.ufpel.edu.br>
2023-07-04 14:27:25 -0300

    exercicio da aula: cadastro, pessoa, data. só iniciado

--- Exercicio4/src/exercicio4/Data.java
+++ Exercicio4/src/exercicio4/Data.java
@@ -9,15 +9,30 @@ package exercicio4;
  * @author User
  */
 public class Data {
-    int dia;
-    int mes;
-    int ano;
+    private int dia;
+    private int mes;
+    private int ano;
+    private static final String[] meses = {"Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"};
     
     public void data(int dia, int mes, int ano){
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
-}    
+    }
+    @Override
+    public String toString(){
+        return (dia < 10 ? "0" + dia : dia) + "/" + (mes <10 ? "0"+ mes : mes) + "/" + (ano <10 ? "0"+ano : ano);
+    }
+    
+    public String extenso(){
+        return (dia < 10 ? "0"+ dia : dia) + "de " + (nomeMes(mes)) + "de " + 
+    }
+    
+    public static String nomeMes(int mes){
+        if(mes > 0 && mes <13)
+            return meses[mes-1];
+        
+    }
 }
 
 

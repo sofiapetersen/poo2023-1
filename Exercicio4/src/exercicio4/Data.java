c202bcc7bee0131de6c11264751413267e1f37fb
Sofia <sppessil@inf.ufpel.edu.br>
2023-07-04 14:02:43 -0300

    exercicio da aula: cadastro, pessoa, data. só iniciado

---
+++ Exercicio4/src/exercicio4/Data.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio4;
+
+/**
+ *
+ * @author User
+ */
+public class Data {
+    int dia;
+    int mes;
+    int ano;
+    
+    public void data(int dia, int mes, int ano){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+}    
+}
+
+
